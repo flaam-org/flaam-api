e820e153409e319ee6f8e66617c50229891a360d
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-08-16 21:10:48 +0530

    initial commit
    Co-authored-by: Mohit Kumar <mohitk0208@users.noreply.github.com>

---
+++ flaam_api/urls.py
@@ -0,0 +1,54 @@
+"""flaam_api URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import debug_toolbar
+from django.contrib import admin
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Flaam API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="",
+        contact=openapi.Contact(email=""),
+        license=openapi.License(name=""),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    # admin
+    path("admin/", admin.site.urls),
+    # debug toolbar
+    path("__debug__/", include(debug_toolbar.urls), name="django_debug_toolbar"),
+    # drf_yasgf
+    path(
+        "swagger",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    # JWT
+    path("api/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    # local
+    path("accounts", include("accounts.urls"), name="accounts"),
+]
