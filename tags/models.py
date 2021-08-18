0a483bb168b29275fcc2e07a661b558356c1461a
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-08-18 18:03:18 +0530

    create tag model

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+        error_messages={"unique": "This Tag already exists."},
+    )
+    description = models.TextField(max_length=300, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
