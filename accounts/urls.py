a95f1c844a72629dfd86a2fe6b9fbed60c63c10a
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-08-18 20:59:20 +0530

    fixed token endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     # JWT
-    path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify", TokenVerifyView.as_view(), name="token_verify"),
+    path("/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("/token/verify", TokenVerifyView.as_view(), name="token_verify"),
 ]
