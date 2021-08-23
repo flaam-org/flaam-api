3511790d9150b9ced407006c077ffc2b1cbf95d4
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-08-23 12:43:45 +0530

    update accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User
+from .models import User, PasswordResetToken
 
 
 class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+    )
     add_fieldsets = (
         (
             None,
@@ -15,18 +23,72 @@ class UserAdmin(BaseUserAdmin):
         ),
     )
     fieldsets = (
-        *BaseUserAdmin.fieldsets,
         (
-            "Custom Fiels",  # group heading
+            None,
             {
                 "fields": (
+                    "username",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
                     "avatar",
                     "status",
                     "description",
-                ),
+                    "favourite_tags",
+                )
+            },
+        ),
+        (
+            "Important dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
             },
         ),
     )
 
 
+class PasswordResetTokenAdmin(admin.ModelAdmin):
+    list_display = (
+        "get_username",
+        "created_at",
+        "token",
+    )
+    search_fields = (
+        "user__username",
+        "user__email",
+        "user__id",
+        "token",
+    )
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    get_username.short_description = "Username"
+    get_username.admin_order_field = "user__username"
+
+
 admin.site.register(User, UserAdmin)
+admin.site.register(PasswordResetToken, PasswordResetTokenAdmin)
