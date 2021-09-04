a5cded0c39c7563e99428897693c24ea3082373d
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-09-04 19:39:00 +0530

    added TagDetailSerializer

--- tags/serializers.py
+++ tags/serializers.py
@@ -3,10 +3,19 @@ from rest_framework import serializers
 from .models import Tag
 
 
-class TagSerializer(serializers.ModelSerializer):
-    """Tag Serializer"""
+class TagDetailSerializer(serializers.ModelSerializer):
+    """Tag Detail Serializer"""
 
     class Meta:
         model = Tag
         fields = ("id", "name", "description", "created_at", "updated_at")
         read_only_fields = ("id", "created_at", "updated_at")
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Tag Serializer"""
+
+    class Meta:
+        model = Tag
+        fields = ("id", "name")
+        read_only_fields = ("id",)
