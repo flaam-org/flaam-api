588bbad856520e7046c33dff7f6a3a0c2287ea18
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-09-05 18:30:07 +0530

    tags: update tag name validators

--- tags/models.py
+++ tags/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+from .validators import TagNameValidator
+
 
 class Tag(models.Model):
     name = models.CharField(
-        max_length=255,
+        max_length=50,
         unique=True,
+        validators=(TagNameValidator,),
+        help_text=(
+            "Tag name must be 1 to 50 characters long. "
+            "It may only contain lowercase alphabets, numbers and hyphens. "
+            "It shouldn't start or end with hyphens. "
+            "It shouldn't contain consecutive hyphens"
+        ),
         error_messages={"unique": "This Tag already exists."},
     )
     description = models.TextField(max_length=300, blank=True)
