ab639586798ceee2f28260b861c3ddbb01c5e4f1
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-09-05 18:50:20 +0530

    tags: add tags list url

--- tags/urls.py
+++ tags/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import TagDetailView
+from .views import TagDetailView, TagListView
 
 urlpatterns = [
+    path("tags", TagListView.as_view()),
     path("tag/<str:name>", TagDetailView.as_view()),
 ]
--- tags/views.py
+++ tags/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import get_object_or_404
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import serializers, status
-from rest_framework.exceptions import APIException, NotFound, ParseError
-from rest_framework.permissions import AllowAny
+from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,12 +12,68 @@ from .serializers import TagDetailSerializer, TagSerializer
 
 class TagDetailView(APIView):
     @swagger_auto_schema(
+        tags=("tags",),
+        operation_summary="Get tag details",
         responses={
-            200: TagDetailSerializer,
-            404: "Not found.",
-        }
+            status.HTTP_200_OK: TagDetailSerializer,
+            status.HTTP_404_NOT_FOUND: "Not found.",
+        },
     )
     def get(self, request: Request, name: str) -> Response:
         tags = get_object_or_404(Tag, name=name)
         serializer = TagDetailSerializer(tags)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class TagListView(APIView):
+    def get_queryset(self):
+        tags = None
+        tag_name = self.request.query_params.get("name", None)
+        tag_ids = self.request.query_params.get("ids", None)
+        print(tag_name, tag_ids)
+        if tag_name:
+            tags = Tag.objects.filter(name__search=tag_name)[:10]
+        elif tag_ids:
+            tags = Tag.objects.filter(id__in=tag_ids.split(","))[:10]
+        return tags
+
+    @swagger_auto_schema(
+        tags=("tags",),
+        operation_summary="Get tags",
+        manual_parameters=(
+            openapi.Parameter(
+                "name",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Search tags by name",
+            ),
+            openapi.Parameter(
+                "ids",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Search tags by id",
+            ),
+        ),
+        responses={
+            status.HTTP_200_OK: TagSerializer,
+        },
+    )
+    def get(self, request: Request) -> Response:
+        serializer = TagSerializer(self.get_queryset(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        tags=("tags",),
+        operation_summary="Create a new tag",
+        request_body=TagDetailSerializer,
+        responses={
+            status.HTTP_201_CREATED: TagDetailSerializer,
+            status.HTTP_400_BAD_REQUEST: "Bad request.",
+            status.HTTP_401_UNAUTHORIZED: "Unauthorized.",
+        },
+    )
+    def post(self, request: Request) -> Response:
+        serializer = TagDetailSerializer(request.data, owner=request.user)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
