ad189e7fbc853ed03972d2f018d8c3eec1439873
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-09-11 23:15:42 +0530

    tags: fixed related fileds and added admin page

--- ideas/admin.py
+++ ideas/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Idea
+
+
+class IdeaAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "created_at", "updated_at", "draft")
+    list_display_links = list_display
+    list_filter = ("created_at", "updated_at", "draft")
+    filter_horizontal = ("tags", "views", "upvotes", "downvotes")
+    search_fields = ("title", "description", "body", "owner__email", "owner__username")
+    ordering = ("-created_at",)
+    readonly_fields = ("id", "created_at", "updated_at")
+
+
+admin.site.register(Idea, IdeaAdmin)
--- ideas/models.py
+++ ideas/models.py
@@ -4,18 +4,23 @@ from django.db import models
 
 class Idea(models.Model):
     title = models.CharField(max_length=255)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
     body = models.TextField()
     # TODO: milestones field
     tags = models.ManyToManyField("tags.Tag", related_name="idea_tags")
     draft = models.BooleanField(default=True)
     upvotes = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name="idea_upvotes"
+        settings.AUTH_USER_MODEL, related_name="idea_upvotes", blank=True
     )
     downvotes = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name="idea_downvotes"
+        settings.AUTH_USER_MODEL, related_name="idea_downvotes", blank=True
     )
-    views = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="idea_views")
+    views = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="idea_views", blank=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"IDEA{self.id}"
