069b40b0bef904ff1fd988c6938038e67aa54e77
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-09-30 16:21:16 +0530

    implementations: added admin

--- implementations/admin.py
+++ implementations/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from djangoql.admin import DjangoQLSearchMixin
 
-# Register your models here.
+from .models import Implementation, ImplementationComment
+
+
+class ImplementationAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = ("id", "owner", "idea", "title", "draft", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at", "draft")
+    search_fields = ("name", "description")
+    readonly_fields = ("created_at", "updated_at")
+
+
+class ImplementationCommentAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = ("id", "owner", "implementation", "body", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("body",)
+    readonly_fields = ("created_at", "updated_at")
+
+
+admin.site.register(Implementation, ImplementationAdmin)
+admin.site.register(ImplementationComment, ImplementationCommentAdmin)
