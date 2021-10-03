f8bb967707db03460f7ee9b707c7026863d27467
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-10-03 19:28:00 +0530

    ideas: reorder serializer values

--- ideas/serializers.py
+++ ideas/serializers.py
@@ -10,13 +10,13 @@ from .models import Idea
 class IdeaSerializer(serializers.ModelSerializer):
     owner_avatar = serializers.SerializerMethodField(read_only=True)
     owner_username = serializers.SerializerMethodField(read_only=True)
+    bookmarked = serializers.SerializerMethodField(read_only=True)
+    viewed = serializers.SerializerMethodField(read_only=True)
     view_count = serializers.IntegerField(read_only=True)
-    implementation_count = serializers.IntegerField(read_only=True)
+    vote = serializers.SerializerMethodField(read_only=True)
     upvote_count = serializers.IntegerField(read_only=True)
     downvote_count = serializers.IntegerField(read_only=True)
-    viewed = serializers.SerializerMethodField(read_only=True)
-    bookmarked = serializers.SerializerMethodField(read_only=True)
-    vote = serializers.SerializerMethodField(read_only=True)
+    implementation_count = serializers.IntegerField(read_only=True)
 
     def get_owner_avatar(self, obj):
         return obj.owner.avatar
@@ -24,16 +24,16 @@ class IdeaSerializer(serializers.ModelSerializer):
     def get_owner_username(self, obj):
         return obj.owner.username
 
-    def get_viewed(self, obj):
-        request = self.context.get("request")
-        if request is not None:
-            return obj.views.filter(pk=request.user.pk).exists()
-        return False
-
     def get_bookmarked(self, obj):
         request = self.context.get("request")
         if request is not None:
             return obj.bookmarked_by.filter(pk=request.user.pk).exists()
+        return False
+
+    def get_viewed(self, obj):
+        request = self.context.get("request")
+        if request is not None:
+            return obj.views.filter(pk=request.user.pk).exists()
         return False
 
     def get_vote(self, obj):
@@ -68,15 +68,15 @@ class IdeaSerializer(serializers.ModelSerializer):
             "description",
             "body",
             "tags",
+            "milestones",
             "draft",
-            "viewed",
             "bookmarked",
+            "viewed",
             "view_count",
             "vote",
-            "implementation_count",
             "upvote_count",
             "downvote_count",
-            "milestones",
+            "implementation_count",
             "created_at",
             "updated_at",
         )
