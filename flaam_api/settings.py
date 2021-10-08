13987db92ca350174da713de6a4e985b96f6f30c
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-10-08 16:42:07 +0530

    settings: removed redundant SIMPLE_JWT config

--- flaam_api/settings.py
+++ flaam_api/settings.py
@@ -182,7 +182,6 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "UPDATE_LAST_LOGIN": True,
-    "BLACKLIST_AFTER_ROTATION": True,
     "LEEWAY": timedelta(seconds=60),
 }
 
