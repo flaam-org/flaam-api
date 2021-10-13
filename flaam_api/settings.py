927b22549241dfae07e47c910fd3778b6a0f9008
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-10-13 20:40:54 +0530

    project: added email config

--- flaam_api/settings.py
+++ flaam_api/settings.py
@@ -174,6 +174,14 @@ SIMPLE_JWT = {
     "LEEWAY": timedelta(seconds=60),
 }
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 SWAGGER_SETTINGS = {
     "DEFAULT_INFO": "flaam_api.urls.api_info",
     "USE_SESSION_AUTH": False,
