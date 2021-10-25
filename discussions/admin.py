f348b1883a379b2d6a15d79d527053e5db41922f
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-10-25 22:13:25 +0530

    discussions: register models on admin

--- discussions/admin.py
+++ discussions/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from djangoql.admin import DjangoQLSearchMixin
 
-# Register your models here.
+from .models import Discussion, DiscussionComment
+
+
+class DiscussionAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = ("id", "owner", "idea", "title", "draft", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at", "draft")
+    search_fields = ("title", "body")
+    readonly_fields = ("created_at", "updated_at")
+
+
+class DiscussionCommentAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = ("id", "owner", "discussion", "body", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("body",)
+    readonly_fields = ("created_at", "updated_at")
+
+
+admin.site.register(Discussion, DiscussionAdmin)
+admin.site.register(DiscussionComment, DiscussionCommentAdmin)
