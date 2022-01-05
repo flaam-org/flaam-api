673c6e0c17039ec4d019931606184f805041546b
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-01-05 17:06:19 +0530

    project: use dj_database_url everywhere

--- flaam_api/settings.py
+++ flaam_api/settings.py
@@ -14,6 +14,8 @@ from datetime import timedelta
 from os import getenv
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,14 +95,7 @@ WSGI_APPLICATION = "flaam_api.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": getenv("DB_NAME"),
-        "USER": getenv("DB_USER"),
-        "PASSWORD": getenv("DB_PASSWORD"),
-        "HOST": getenv("DB_HOST"),
-        "PORT": getenv("DB_PORT"),
-    }
+    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3")
 }
 
 
@@ -268,7 +263,6 @@ if DEBUG:
 
 if getenv("HEROKU_ENVIRONMENT"):
 
-    import dj_database_url
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
 
